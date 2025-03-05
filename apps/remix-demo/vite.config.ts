1421ce07f55f67483d1af234d8b8a57b732922aa
richard-hefner_pure <richard.hefner@purestorage.com>
2025-03-04 17:36:14 -0800

    wip: working on sourcemaps, updated deps + added inspect plugin for vite

--- apps/remix-demo/vite.config.ts
+++ apps/remix-demo/vite.config.ts
@@ -1,15 +1,36 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import Inspect from 'vite-plugin-inspect';
 
 const port =
   typeof process.env.PORT === 'string' && process.env.PORT.match(/^\d+$/)
     ? parseInt(process.env.PORT)
     : 5173;
 
-export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
-  server: {
-    port,
-  },
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+
+  return {
+    build: {
+      sourcemap: true,
+    },
+    css: {
+      devSourcemap: isDev,
+    },
+    esbuild: {
+      sourcemap: true,
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        sourcemap: true,
+        sourcesContent: true,
+      },
+    },
+    plugins: [reactRouter(), tsconfigPaths(), Inspect()],
+    server: {
+      port,
+      sourcemap: true,
+    },
+  };
 });
